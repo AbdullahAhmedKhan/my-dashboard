000117eaa8ac82db872c4f668804966741eb5b9c
Abdullah Ahammed Khan <abdullahahmedkhan@outlook.com>
2025-09-08 23:59:28 +0600

    3 Chart added, Bar Chart, Area Chart & Pie Chart

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,30 @@
-import { Button } from "@/components/ui/button";
-import { CircleArrowOutUpRight } from "lucide-react";
-import Image from "next/image";
+import AppAreaChart from "@/components/AppAreaChart";
+import AppBarChart from "@/components/AppBarChart";
+import AppPieChart from "@/components/AppPieChart";
+import CardList from "@/components/CardList";
 
-export default function Home() {
+const Homepage = () => {
   return (
-    <div className="p-20">
+    <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+        <AppBarChart />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg"> 
+        <CardList title="Latest Transactions" />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg">
+        <AppPieChart />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg">
+        Test
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg g:col-span-2 xl:col-span-1 2xl:col-span-2">
+        <AppAreaChart />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg"> <CardList title="Popular Content" /></div>
     </div>
 
   );
 }
+
+export default Homepage;
---
+++ src/components/CardList.tsx
@@ -0,0 +1,115 @@
+import Image from "next/image";
+import { Card, CardContent, CardFooter, CardTitle } from "./ui/card";
+import { Badge } from "./ui/badge";
+
+const popularContent = [
+    {
+        id: 1,
+        title: "JavaScript Tutorial",
+        badge: "Coding",
+        image: "https://images.pexels.com/photos/1181671/pexels-photo-1181671.jpeg?auto=compress&cs=tinysrgb&w=600",
+        count: 4300
+    },
+    {
+        id: 2,
+        title: "React Guide",
+        badge: "Coding",
+        image: "https://images.pexels.com/photos/1181671/pexels-photo-1181671.jpeg?auto=compress&cs=tinysrgb&w=600",
+        count: 4300
+    },
+    {
+        id: 3,
+        title: "UI Design Basics",
+        badge: "Design",
+        image: "https://images.pexels.com/photos/1181671/pexels-photo-1181671.jpeg?auto=compress&cs=tinysrgb&w=600",
+        count: 4300
+    },
+    {
+        id: 4,
+        title: "Advanced CSS",
+        badge: "Design",
+        image: "https://images.pexels.com/photos/1181671/pexels-photo-1181671.jpeg?auto=compress&cs=tinysrgb&w=600",
+        count: 4300
+    },
+    {
+        id: 5,
+        title: "Node.js Crash Course",
+        badge: "Coding",
+        image: "https://images.pexels.com/photos/1181671/pexels-photo-1181671.jpeg?auto=compress&cs=tinysrgb&w=600",
+        count: 4300
+    },
+    {
+        id: 6,
+        title: "Python for Beginners",
+        badge: "Coding",
+        image: "https://images.pexels.com/photos/1181671/pexels-photo-1181671.jpeg?auto=compress&cs=tinysrgb&w=600",
+        count: 4300
+    }   
+]
+
+const latestTransactions = [
+    {
+        id: 1,
+        title: "Subscription Renewal",
+        badge: "John Doe",
+        image: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=600",
+        count: 1400
+    },
+    {
+        id: 2,
+        title: "New Purchase",
+        badge: "Jane Smith",
+        image: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=600",
+        count: 1200
+    },
+    {
+        id: 3,
+        title: "Refund Processed",
+        badge: "Alice Johnson",
+        image: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=600",
+        count: 1000
+    },
+    {
+        id: 4,
+        title: "Subscription Renewal",
+        badge: "John Doe",
+        image: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=600",
+        count: 1400
+    },
+    {
+        id: 5,
+        title: "Payment Received",
+        badge: "John Doe",
+        image: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=600",
+        count: 1400
+    }
+]
+
+const CardList = ({title}: {title: string}) => {
+    const list = title === "Popular Content" ? popularContent : latestTransactions;
+  return (
+    <div className="">
+        <h1 className="text-lg font-medium mb-6">{title}</h1>
+        <div className="flex flex-col gap-2">
+            {list.map((item) => (
+                <Card key={item.id} className="flex-row items-center justify-between gap-4 p-4">
+                <div className="w-12 h-12 rounded-sm relative overflow-hidden ">
+                    <Image src={item.image} alt={item.title} fill className="object-cover" />
+                </div>
+                <CardContent className="p-0">
+                    <CardTitle className="text-sm font-medium">
+                        {item.title}
+                    </CardTitle>
+                    <Badge variant="secondary">{item.badge}</Badge>
+                </CardContent>
+                <CardFooter>
+                    {item.count/1000}k
+                </CardFooter>
+                    </Card>
+            ))}
+        </div>
+    </div>
+  )
+}
+
+export default CardList
